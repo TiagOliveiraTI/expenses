8b710b32a7b5ea3e9bf4d1cad13ce2d4cb4b85a8
Tiago A. Oliveira <contato@otao.dev.br>
2023-07-12 21:18:21 -0300

    ✨ send data between components

--- lib/components/transaction_form.dart
+++ lib/components/transaction_form.dart
@@ -4,7 +4,9 @@ class TransactionForm extends StatelessWidget {
   final titleController = TextEditingController();
   final valueController = TextEditingController();
 
-  TransactionForm({super.key});
+  final void Function(String, double) onSubmit;
+
+  TransactionForm(this.onSubmit);
 
   @override
   Widget build(BuildContext context) {
@@ -27,8 +29,10 @@ class TransactionForm extends StatelessWidget {
               children: [
                 TextButton(
                   onPressed: () {
-                    print(titleController.text);
-                    print(valueController.text);
+                    final title = titleController.text;
+                    final value = double.tryParse(valueController.text) ?? 0.0;
+
+                    onSubmit(title, value);
                   },
                   style: TextButton.styleFrom(
                     foregroundColor: Colors.purple,
